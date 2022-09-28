65db716263584c455b721d91d7988235e7f839c6
joehan1303 <joehanm10@gmail.com>
2022-09-28 10:46:50 +0530

    created react facts app

---
+++ first-react-project/src/components/main.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function Main(){
+    return(
+        <main className="main">
+            <h1 className="main-title">
+                Fun Facts about React
+            </h1>
+            <ul className="main-facts">
+                <li>Was First released in 2013</li>
+                <li>Was originally created by Jordan Walker</li>
+                <li>Has well over 100k stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+
+        
+        )
+}
