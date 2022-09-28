8a5a675198a3189df08dc2541ba5dea2d01f6821
joehan1303 <joehanm10@gmail.com>
2022-09-28 13:01:58 +0530

    Update digital-business-card

--- digital-business-card/src/App.js
+++ digital-business-card/src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
+import Footer from './components/footer';
+import Face from './Images/Rectangle 90.jpg';
+import Email from './Images/Mail.svg'
+import linkedln from './Images/linkedin.svg'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <body className="App">
+      <img src={Face} className="Face" ></img>
+      <div className="App-Main">
+        <text className="App-Info-Title">Joehan Misquitta</text>
+        <text className="App-Info-Designation">Frontend Developer</text>
+        <text className="App-Info-Website">joehanmisquitta.com</text>
+        <button className="App-Info-Email">
+          <img src={Email}></img>
+          <text>Email</text>
+        </button>
+        <button className="App-Info-Linkedln">
+          <img src={linkedln}></img>
+          <text>Linkedln</text>
+        </button>
+      </div>
+      <div>
+        <Footer/>
+      </div>
+    </body>
   );
 }
-
-export default App;
--- digital-business-card/src/components/footer.js
+++ digital-business-card/src/components/footer.js
@@ -1,11 +1,17 @@
-function App() {
+import React from "react"
+
+
+function Footer() {
     return (
       <div className="App-Footer">
         <header className="App-header-Footer">
-  
+            <image></image>
+            <image></image>
+            <image></image>
+            <image></image>
         </header>
       </div>
     );
   }
   
-  export default App;
+  export default Footer;
