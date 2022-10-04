cbb8a4498aed971ba3778d8b29fe37d521561998
joehan1303 <joehanm10@gmail.com>
2022-10-04 19:59:30 +0530

    Added Props

--- airbnb-experience/src/App.js
+++ airbnb-experience/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
-import NavBar from "./components/Navbar"
-import Hero from "./components/Hero"
+import NavBar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from './components/card';
+import Swimmer from "./images/swimmer.svg";
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Hero/>
+      <Card
+        img={Swimmer}
+        rating = "5.0"
+        no_of_ratings = {6}
+        country = "USA"
+        description = "Life lessons with Katie Zaferes"
+        cost = {136}
+        unit = "Person"
+      />
     </div>
   );
 }
