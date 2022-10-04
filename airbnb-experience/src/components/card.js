ac426acd87cf4f2130dc3b62dcb71ecb3258213a
joehan1303 <joehanm10@gmail.com>
2022-10-04 20:00:15 +0530

    Added props

--- airbnb-experience/src/components/card.js
+++ airbnb-experience/src/components/card.js
@@ -1,1 +1,21 @@
 import React from 'react';
+import star from "../images/Star.svg"
+
+export default function Card(props){
+
+    return(
+        <div className="Card">
+            <img src={props.img} className="Card-Img" alt="Card"/>
+            <div className="Card-Stats">
+                <img src={star} className="Card-Star" alt="star"/>
+                <span>{props.rating}</span>
+                <span className="grey">({props.no_of_ratings})•</span>
+                <span className="grey">{props.country}</span>
+            </div>
+            <p>{props.description}</p>
+            <p>From <span className="bold">${props.cost}/</span>{props.unit}</p>
+
+        </div>
+        
+    )
+}
